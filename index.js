2ba4b67f516b04bb69759fe9b986418e08fcdeff
Kento Nishi <kento24gs@outlook.com>
2021-02-13 23:02:38 +0000

    Added message

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 8080;
 app.post('/github', function (req, res) {
   res.status(200);
   if(req.body.ref === 'refs/heads/master' ) {
+    console.log('Pulling new changes and rebooting...');
     exec('git reset --hard HEAD; git fetch --all; git pull;');
   }
 });
