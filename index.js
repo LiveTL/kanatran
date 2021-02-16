24facecccf88b70cb6985bf31fab1d515b8894dc
Kento Nishi <kento24gs@outlook.com>
2021-02-16 22:30:51 +0000

    Print command outputs

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ app.post('/github', (req, res) => {
   res.end();
   if (req.body.ref === 'refs/heads/master' ) {
     console.log('Pulling new changes and rebooting if neccessary...');
-    exec('make update');
-    console.log('Done updating');
+    exec('make update').stdout.pipe(process.stdout);
   }
 });
 
