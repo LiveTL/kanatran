487f4069c650ccdfa901692371f149984ccca9d7
Kento Nishi <kento24gs@outlook.com>
2021-03-13 07:51:15 +0000

    make update &

--- controller/src/index.js
+++ controller/src/index.js
@@ -30,7 +30,7 @@ app.post('/github', (req, res) => {
   res.end();
   if (req.body.ref === 'refs/heads/master' ) {
     console.log('Pulling new changes and rebooting if neccessary...');
-    exec('cd ..; make update').stdout.pipe(process.stdout);
+    exec('cd ..; make update &').stdout.pipe(process.stdout);
   }
 });
 io.on('connection', (socket) => {
