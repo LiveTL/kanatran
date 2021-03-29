0653d904f795a0ea32676d81dd024c010c24f5e5
r2dev2 <ronak.badhe@gmail.com>
2021-03-29 13:05:33 -0700

    remove even more ded code

--- watcher/src/server/server.py
+++ watcher/src/server/server.py
@@ -1,18 +1,10 @@
 import asyncio
-import functools
-import io
 import os
-import re
-import sys
 from pathlib import Path
 from pprint import pprint
 from threading import Thread
 from typing import Optional
 
-import aiohttp
-import requests
-import translators as ts
-import youtube_dl
 from autoselenium import chrome
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
@@ -25,7 +17,6 @@ static = Path(__file__).resolve().parent / "../public"
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory=static), name="static")
-session = aiohttp.ClientSession()
 
 
 class Disp(Display):
@@ -41,29 +32,9 @@ display.start()
 # print("Using ch", os.environ.get("CHANNEL_ID"))
 # ytl = YTLiveService(os.environ.get("CHANNEL_ID", "UCZlDXzGoo7d44bwdNObFacg"))
 
-translate = ts.bing
-
-
-async def translate(jap: str) -> Optional[str]:
-    if 0:
-        loop = asyncio.get_event_loop()
-        return await loop.run_in_executor(None, lambda: ts.bing(jap))
-    return None
-
 
 async def get_video_id() -> Optional[str]:
     return os.environ.get("VIDEO")
-
-
-def launch_selenium() -> None:
-    global web  # Avoid garbage collection
-    chrome.setup_driver()
-    web = chrome.get_selenium(True)
-    try:
-        web.switch_to.window("1")
-    except Exception:
-        pass
-    web.get("http://localhost:42069/static/index.html")
 
 
 @app.get("/timestamp", response_model=Timestamp)
